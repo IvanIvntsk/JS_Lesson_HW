cc5ce8969d6e30432b635639219c046ef5fa0086
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-05-01 21:50:53 +0300

    task1

---
+++ HW1/JSl1.js
@@ -0,0 +1,24 @@
+console.log(`Hello Worl`)
+
+let s1 = `Hello`
+let s2 = `OWU`
+
+let a1 = 1
+let a2 = 10
+let a3 = -999
+let a4 = 123
+let a5 = 3.14
+let a6 = 2.7
+let a7 = 16
+
+console.log(s1)
+console.log(s2)
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a4)
+console.log(a5)
+console.log(a6)
+console.log(a7)
+   // 'com', 'ua', true, false
