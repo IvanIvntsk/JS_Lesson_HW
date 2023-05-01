a3d6417691043e53f167253ea0cf3be9f9f7f100
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-05-01 22:23:02 +0300

    task1 task2 task3

--- HW1/JSl1.js
+++ HW1/JSl1.js
@@ -1,7 +1,7 @@
-console.log(`Hello Worl`)
-
 let s1 = `Hello`
 let s2 = `OWU`
+let s3 = `com`
+let s4 = `ua`
 
 let a1 = 1
 let a2 = 10
@@ -11,8 +11,14 @@ let a5 = 3.14
 let a6 = 2.7
 let a7 = 16
 
+let check1 = true
+let check2 = false
+
+
 console.log(s1)
 console.log(s2)
+console.log(s3)
+console.log(s4)
 
 console.log(a1)
 console.log(a2)
@@ -21,4 +27,28 @@ console.log(a4)
 console.log(a5)
 console.log(a6)
 console.log(a7)
-   // 'com', 'ua', true, false
+
+console.log(check1)
+console.log(check2)
+
+// ---------------------------------------------------2-------------------------------
+
+let firstName = `Ivan`
+let middleName = `Igorovych`
+let lastName = `Ivanytskyi`
+
+let user = `${lastName} ${firstName} ${middleName} `
+console.log(user)
+
+//-----------------------3------------------
+
+let b1 = 100;
+let b2 = '100';
+let b3 = true;
+
+console.log(typeof b1)
+console.log(typeof b2)
+console.log(typeof b3)
+
+//--------------------4-------------
+
