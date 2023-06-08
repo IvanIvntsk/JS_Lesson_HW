205c47e49bcb7862db4f8b391258fc45dbf3f0f0
IvavIvntsk <ivan.ivanytskyii@gmail.com>
2023-06-08 19:27:29 +0300

    corect

--- HW12__url_http_regexp/HW12_url_http_regexp.js
+++ HW12__url_http_regexp/HW12_url_http_regexp.js
@@ -10,6 +10,8 @@ fetch(`http://jsonplaceholder.typicode.com/users`)
 .then(value => console.log(value))
 
 //     вивести їх id + name списком та додати посилання з href = user-details.html?id=XXX (замість ХХХ - айді юзера)
+// при кліку на посилання перехід на відповідну сторінку, на якій буде вся інформація про користувача (всі 15 полів)
+// отримана через додатковий запит (   ХХХ - айді користувача)
 fetch(`http://jsonplaceholder.typicode.com/users`)
     .then(response => response.json())
     .then(value => {
@@ -28,7 +30,7 @@ fetch(`http://jsonplaceholder.typicode.com/users`)
 
         })
 
-        console.log(usersList)
+        // console.log(usersList)
     })
 
 
